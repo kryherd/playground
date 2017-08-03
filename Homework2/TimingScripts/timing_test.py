361205530151d2b2d1e5b48061eca367beb5e4c3
Kayleigh Ryherd <kayleigh.ryherd@uconn.edu>
2017-08-03 14:10:11 -0400

    Re-submission with timing scripts

---
+++ Homework2/TimingScripts/timing_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# import modules
+import re #regex module
+
+# open file
+with open("/Users/Kayleigh/Dropbox/IBRAIN/Homework2/sub-03/ses-test/func/sub-03_ses-test_task-linebisection_events.tsv", 'r') as f:
+    s=f.read()
+
+# setup dictionary
+timing_dic = {"LCor": [], "LIncor": [], "DResp": [], "LNR": [], "DNR": []}
+
+regex = re.compile(r"([\d.]+)+\t([\d.]+)+\t([\d.]+)\t([\w]+)", re.MULTILINE)
+
+matches = regex.finditer(s)
+
+for m in matches:
+	if m.groups()[3] == "Correct_Task":
+		timing_dic["LCor"].append(m.groups()[0])
+	elif m.groups()[3] == "Incorrect_Task":
+		timing_dic["LIncor"].append(m.groups()[0])
+	elif m.groups()[3] == "Response_Control":
+		timing_dic["DResp"].append(m.groups()[0])
+	elif m.groups()[3] == "No_Response_Task":
+		timing_dic["LNR"].append(m.groups()[0])
+	elif m.groups()[3] == "No_Response_Control":
+		timing_dic["DNR"].append(m.groups()[0])
+
+with open("timing_LCor.1D","w") as f:
+	f.write(' '.join(timing_dic["LCor"]))
+with open("timing_LIncor.1D","w") as f:
+	f.write(' '.join(timing_dic["LIncor"]))
+with open("timing_DResp.1D","w") as f:
+	f.write(' '.join(timing_dic["DResp"]))
+with open("timing_LNR.1D","w") as f:
+	f.write(' '.join(timing_dic["LNR"]))
+with open("timing_DNR.1D","w") as f:
+	f.write(' '.join(timing_dic["DNR"]))
